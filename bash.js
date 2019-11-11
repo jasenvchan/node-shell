ae6d54c7b36b3233f28c7fa28c86b7b8342cf981
James Shen <jamesshen@James-MBPro19.local>
2019-11-11 12:41:59 -0500

    pwd command

--- bash.js
+++ bash.js
@@ -1,17 +1,18 @@
+const pwd = require('./pwd.js')
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   const commands = {
-  	pwd: process.cwd(),
-
+  	pwd: pwd.pwd,
   };
 
   if(commands[cmd] === undefined){
   	process.stdout.write('\nNo command found.');
   }
   else process.stdout.write(`\n${commands[cmd]}`);
-  
+
   process.stdout.write('\nprompt > ');
 });
