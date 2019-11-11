2d3825122968d9ff365ad3f74f1806a92ae75346
jasenvchan <jachan@gwu.edu>
2019-11-11 12:29:33 -0500

    got cwd working

--- bash.js
+++ bash.js
@@ -3,6 +3,15 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + cmd);
+  const commands = {
+  	pwd: process.cwd(),
+
+  };
+
+  if(commands[cmd] === undefined){
+  	process.stdout.write('\nNo command found.');
+  }
+  else process.stdout.write(`\n${commands[cmd]}`);
+  
   process.stdout.write('\nprompt > ');
 });
